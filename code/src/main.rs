03559dec13e8fb0d5efea8065f7a3be9614e20fd
sunwenming <645187582@qq.com>
2023-01-12 14:02:32 +0800

    1

---
+++ code/src/main.rs
@@ -0,0 +1,20 @@
+// 逛街时穿的衣服
+struct ClothesShop {}
+
+impl ClothesShop {
+    fn Style(self) {
+        println!("逛街时的装扮")
+    }
+}
+
+// 工作时穿的衣服
+struct CloseWork {}
+impl CloseWork {
+    fn Style(self) {
+        println!("工作时的装扮")
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
